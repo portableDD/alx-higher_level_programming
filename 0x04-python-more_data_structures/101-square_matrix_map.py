364d812852b60f4034bf982949a49adcc194494c
portable <emmanueltemitopedorcas20@gmail.com>
2021-09-17 15:34:09 +0100

    4-python-more-data

---
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def square_matrix_map(matrix=[]):
+    return list(map(lambda j: list(map(lambda i: i**2, j)), matrix))
+    
---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    return {x: i * 2 for x, i in a_dictionary.items()}
+    
